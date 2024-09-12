9583547f8dc7d20e55d2b53a64d3d1e878e39423
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-09-12 15:45:30 +1000

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -36,6 +37,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>
+			<Analytics />
 		</html>
 	);
 }
